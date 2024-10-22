b37299c41d8120eff67493165fd81bed502d692d
wolfram0407 <77099657+wolfram0407@users.noreply.github.com>
2024-10-22 18:50:51 +0900

    Feat - docker-compose 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,15 @@
-import { Module } from '@nestjs/common';
+import {Module} from '@nestjs/common';
 
-import { ConfigModule } from '@nestjs/config';
-import { MongooseModule } from '@nestjs/mongoose';
-import { CatsModule } from './cats/cats.module';
+import {ConfigModule} from '@nestjs/config';
+import {MongooseModule} from '@nestjs/mongoose';
+import {CatsModule} from './cats/cats.module';
 
-import configuration, { validationSchema } from './config/configuration';
-import { AuthModule } from './auth/auth.module';
-import { UsersModule } from './users/users.module';
-import { LockerModule } from './locker/locker.module';
-import { LockerTypeModule } from './locker-type/locker-type.module';
-import { CustomerModule } from './customer/customer.module';
+import configuration, {validationSchema} from './config/configuration';
+import {AuthModule} from './auth/auth.module';
+import {UsersModule} from './users/users.module';
+import {LockerModule} from './locker/locker.module';
+import {LockerTypeModule} from './locker-type/locker-type.module';
+import {CustomerModule} from './customer/customer.module';
 
 @Module({
   imports: [
